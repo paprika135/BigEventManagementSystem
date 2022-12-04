71e882428f3bcaa86542ddf56a21025167488973
weizhu <1599812825@qq.com>
2022-12-04 15:39:26 +0800

    完成了首页的安全性校验

--- assets/js/BaseAPI.js
+++ assets/js/BaseAPI.js
@@ -1,4 +1,13 @@
 //里面有个参数会接收$.ajax里的配置对象。
 $.ajaxPrefilter((options) =>{
     options.url = 'http://www.liulongbin.top:3007' + options.url;
+    options.complete = function(options){
+        // console.log(options.responseJSON);
+        if(options.responseJSON.status === 1 && options.responseJSON.message === '身份认证失败！'){
+            //  1. 强制清空 token
+        localStorage.removeItem('token')
+        // 2. 强制跳转到登录页面
+        location.href = '/login.html'
+        }
+    };
 });
--- assets/js/index.js
+++ assets/js/index.js
@@ -8,7 +8,7 @@ $(function(){
             Authorization:localStorage.getItem('token') || ''
         },success:function(res){
             if(res.status !== 0){
-                return latui.layer.msg('获取用户信息失败！');
+                return layui.layer.msg('获取用户信息失败！');
             }
             //获取信息成功渲染头像
             renderAvatar(res.data)
@@ -20,14 +20,26 @@ $(function(){
 
    function renderAvatar(user){
     //这里还要写欢迎文本
-    console.log(user);
-    
     var name = user.nickname || user.username;
     var frist = name[0].toUpperCase();
     $('#welcome').html('欢迎&nbsp;&nbsp;' + name);
     if(user.user_pic === null){
         $('.layui-nav-img').hide();
         $('.text-avatar').html(frist);
-    }
+    }else{
+        $('.layui-nav-img').attr('src',user.user_pic).show();
+        $('.text-avatar').hide();
+    };
    };
+
+   //实现退出功能
+   $('#quit').on('click',function(){
+    layer.confirm('确定退出吗？', {icon: 3, title:'提示'}, function(index){
+        //do something
+        //点击过这个按钮后强制返回登录页面，并清除token值。
+        localStorage.removeItem('token');
+        location.href = '/login.html';
+        layer.close(index);
+      });
+   });
 });
